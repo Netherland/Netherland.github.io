e8ea90d93e766e0038f49c9eecfafd65ddd870d2
Estee <teenether@live.nl>
2016-03-26 16:11:33 +0100

    Version 1.0 Release

--- index.php
+++ index.php
@@ -94,23 +94,23 @@
           <div class="col s12 m4">
             <div class="icon-block">
               <div class="iconcircle">
-                <h2 class="center black-text iconsymbol"><i class="material-icons">call</i></h2></div>
+                <h2 class="center blotus-text iconsymbol"><i class="material-icons">call</i></h2></div>
               <h5 class="center">Telefonisch bestellen</h5>
 
-              <p class="light center">U kunt ons telefonisch bereiken op<br><span class="infoimportant black-text">050-3143366</span><br><a class="btn-large moremargins waves-effect waves-light lotuskleur lighten-1 black-text lotuskleur hide-on-med-and-up z-depth-1" href="tel:0503143366">Bel Nu</a><br></p>
+              <p class="light center">U kunt ons telefonisch bereiken op<br><span class="infoimportant black-text">050-3143366</span><br><a class="btn-large moremargins waves-effect waves-light blotus-bg lighten-1 white-text hide-on-med-and-up z-depth-1" href="tel:0503143366">Bel Nu</a><br></p>
             </div>
           </div>
 
           <div class="col s12 m4">
             <div class="icon-block">
               <div class="iconcircle">
-                <h2 class="center black-text iconsymbol"><i class="material-icons">web</i></h2></div>
+                <h2 class="center blotus-text iconsymbol"><i class="material-icons">web</i></h2></div>
               <h5 class="center">Online bestellen</h5>
 
               <p class="light center">U kunt met de onderstaande knop bestellen op de website
               </p>
               <div class="row center">
-                <a class="btn-large waves-effect waves-light lotuskleur lighten-1 black-text lotuskleur hide-on-med-and-up bestelknop z-depth-1 moremargins" href="http://bestellen.blauwe-lotus.nl/">Bestel Nu</a>
+                <a class="btn-large waves-effect waves-light blotus-bg lighten-1 white-text hide-on-med-and-up bestelknop z-depth-1 moremargins" href="http://bestellen.blauwe-lotus.nl/">Bestel Nu</a>
               </div>
 
             </div>
@@ -120,20 +120,20 @@
           <div class="col s12 m4">
             <div class="icon-block">
               <div class="iconcircle">
-                <h2 class="center black-text iconsymbol"><i class="material-icons">business</i></h2></div>
+                <h2 class="center blotus-text iconsymbol"><i class="material-icons">business</i></h2></div>
               <h5 class="center">Zelf afhalen</h5>
 
               <p class="light center">U kunt ook zelf afhalen op het volgende adres<br><span class="infoimportant">Nieuwe Ebbingestraat 111<br>9712 NJ Groningen</span></p>
               <div class="row center nomargins">
-                <a class="btn-large waves-effect waves-light lotuskleur lighten-1 black-text lotuskleur ss hide-on-med-and-up z-depth-1 moremargins" href="#map">Kaart</a>
+                <a class="btn-large waves-effect waves-light blotus-bg lighten-1 white-text ss hide-on-med-and-up z-depth-1 moremargins" href="#map">Kaart</a>
               </div>
             </div>
           </div>
         </div>
         <div class="row hide-on-small-only center">
-          <div class="col s4"><a class="btn-large waves-effect waves-light lotuskleur lighten-1 black-text lotuskleur tooltipped z-depth-1" data-position="top" data-delay="50" data-tooltip="werkt alleen op mobiel" href="tel:0503143366">Bel Nu</a></div>
-          <div class="col s4"><a class="btn-large waves-effect waves-light lotuskleur lighten-1 black-text lotuskleur bestelknop z-depth-1" href="http://bestellen.blauwe-lotus.nl/">Bestel Nu</a></div>
-          <div class="col s4"><a class="btn-large waves-effect waves-light lotuskleur lighten-1 black-text lotuskleur ss z-depth-1" href="#map">Kaart</a></div>
+          <div class="col s4"><a class="btn-large waves-effect waves-light lighten-1 blotus-bg white-text tooltipped z-depth-1" data-position="top" data-delay="50" data-tooltip="werkt alleen op mobiel" href="tel:0503143366">Bel Nu</a></div>
+          <div class="col s4"><a class="btn-large waves-effect waves-light lotuskleur lighten-1 blotus-bg white-text lotuskleur bestelknop z-depth-1" href="http://bestellen.blauwe-lotus.nl/">Bestel Nu</a></div>
+          <div class="col s4"><a class="btn-large waves-effect waves-light lotuskleur lighten-1 blotus-bg white-text lotuskleur ss z-depth-1" href="#map">Kaart</a></div>
         </div>
       </div>
     </div>
@@ -210,7 +210,7 @@
     <div class="container pictextcontainer z-depth-1">
       <div class="row">
         <div class="col m5 s12 center">
-          <h5>Veel ervaring</h5>
+          <h5 class="lotuslogo-text largetext">Veel ervaring</h5>
           <p>
             Al sinds 1989 mag de Blauwe Lotus zich verheugen op een groeiend aantal tevreden klanten.<br>
             <br> In dat jaar werd gestart met de toen voor Nederland unieke bezorgservice van Chinese maaltijden. Hiermee werd de Blauwe Lotus de eerste bezorgchinees van Nederland.
@@ -226,7 +226,7 @@
           <img src="img/inlay2.jpg" class="z-depth-1 full-width" alt="Plaatje">
         </div>
         <div class="col m5 s12 center">
-          <h5>Erg bekend</h5>
+          <h5 class="lotuslogo-text largetext">Erg bekend</h5>
           <p>
             Vele huishoudens, bedrijven en overheidsinstanties weten inmiddels via de telefoon en sinds kort ook via internet de weg te vinden naar een smakelijke maaltijd.<br>
             <br>De Blauwe Lotus loopt ook nu nog steeds voorop met een uitgebreid menu van heerlijke Chinese maaltijden, bereid met de beste verse ingredi&#235;nten.<br>
@@ -235,7 +235,7 @@
       </div>
       <div class="row">
         <div class="col m5 s12 center">
-          <h5>Goede service</h5>
+          <h5 class="lotuslogo-text largetext">Goede service</h5>
           <p>
             Of u nu kiest voor onze afhaal-, bezorg-, of cateringsmaaltijden: bij de Blauwe Lotus bent u altijd verzekerd van goede service voor een zeer redelijke prijs.<br> Daarom is de Blauwe Lotus al ruim 20 jaar de nummer een van Groningen voor Chinese
             thuisbezorgen..
@@ -248,8 +248,8 @@
     </div>
     <div class="container pictextcontainer moremargins moremarginstopsmall z-depth-1">
       <div class="row center">
-        <h2 class="stars"><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate black-text center"></i></h2>
-        <h4>Gemiddeld met 4 sterren beoordeeld</h4>
+        <h2 class="stars fnomargins"><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate yellow-text center starsg"></i><i class="mdi-action-star-rate black-text center"></i></h2>
+        <h5 class="fnomargins">Gemiddeld met 4 sterren beoordeeld</h5><a href="http://bestellen.blauwe-lotus.nl/beoordelingen.php">Na >150 reacties</a>
       </div>
     </div>
 
@@ -288,55 +288,14 @@
 
         <div class="row">
           <div class="col s12 center">
-            <h3><i class="mdi-action-star-rate brown-text center"></i></h3>
+            <h3><i class="mdi-maps-place blotus-text center"></i></h3>
             <h4>Vind ons op de kaart</h4>
 
           </div>
         </div>
         <div class="overlay"></div>
         <div class="center moremargins z-depth-3" id="map"></div>
-        <div class="contactform center">
-          <form class="col s8 offset-s2">
-
-            <div class="row">
-              <div class="input-field col s12 ">
-                <i class="material-icons prefix ">account_circle</i>
-                <input id="name" type="text" class="validate">
-                <label for="name">Naam</label>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">email</i>
-                <input id="email" type="email" class="validate">
-                <label for="email ">E-Mail</label>
-              </div>
-            </div>
-            <!--row-->
-
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">subject</i>
-                <input id="message-sub" type="email" class="validate">
-                <label for="message-sub">Titel</label>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">mode_edit</i>
-                <textarea id="text_area" class="materialize-textarea"></textarea>
-                <label for="text_area">Tekst</label>
-              </div>
-            </div>
-
-
-            <div>
-              <a class="waves-effect waves-light btn light-blue lighten-1">Stuur<i class="mdi-content-send right"></i></a>
-            </div>
-          </form>
-        </div>
+        <div class="center"> <a class="btn-large waves-effect waves blotus-bg lighten-1 white-text z-depth-1 moremargins" href="https://maps.google.com/?daddr=Nieuwe%20Ebbingestraat+111+Groningen">Routebeschrijving</a></div>
       </div>
     </div>
 
@@ -365,9 +324,9 @@
     <div class="footer-copyright">
       <div class="container center">
         <div class="row">
-          <div class="col s6 black-text valign-wrapper">
-            Made with <span class="hearts valign">❤</span> in <span class="light">050</span></div>
-          <div class="col s6 black-text"> Built with <a href="http://materializecss.com/" class="materialize">Materialize</a>
+          <div class="col s6 black-text valign-wrapper center">
+            Eet Smakelijk!</div>
+          <div class="col s6 black-text"> Designed by <a href="http://webbuildr.nl/" class="blotus-text">WebBuildR</a>
           </div>
         </div>
       </div>
